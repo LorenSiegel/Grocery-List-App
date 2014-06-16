56671b24eea169f5a384bf2f0830d9ffb5abc198
Loren Siegel <lorensiegel@Lorens-iMac.local>
2014-06-16 18:52:02 -0500

    no strike through

--- jss/app.js
+++ jss/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 //ADD Button with Remove Button
 $('#btnName').click(function(){
-    var text = $('#inputName').val() + '<button id="x">x</button>';
+    var text = $('#inputName').val() + '<button id="x">x</button>'+'<input id="check" type="checkbox">';
     if(text.length){
         $('<li />', {html: text}).appendTo('ul.justList')
     }
@@ -12,16 +12,18 @@ $('ul').on('click','button' , function(el){
     $(this).parent().remove()
  });
 
-$("ul").click(function(){
-	$(this).find("li").wrap("<strike>");
-})
+$('.justList input').on('click', function () {
+  var $li = $(this).closest('li');
+  $li.wrap('<strike>')
+  console.log($li);
+  });
 });
+
   
 	
+//event that marks throug a list item 
 
 
-//event that marks throug a list item - double click
 
-//event that fades out unwanted items
 
 

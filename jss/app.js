5aea92f3bd81ad329b68e6b3b7f20d1e4a69bb74
Loren Siegel <lorensiegel@Lorens-iMac.local>
2014-06-16 09:57:24 -0500

    ADD and Remove Buttons

--- jss/app.js
+++ jss/app.js
@@ -1,57 +1,25 @@
 $(document).ready(function() {
-$(".ryu").mouseenter(function(){
-	$(".ryu-still").hide();
-	$(".ryu-ready").show();
-	$(".ryu-cool").hide();
-	$(".ryu-throwing").hide();
-	})
-	.mouseleave(function(){
-		$(".ryu-ready").hide();
-		$(".ryu-still").show();
-		$(".ryu-cool").hide();
-		$(".ryu-throwing").hide();
-	})
-	.mousedown(function(){
-		playHadouken();
-		$(".ryu-cool").hide();
-		$(".ryu-still").hide();
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").show();
-		$(".hadouken").show();
-		$(".hadouken").finish().show()
-		.animate (
-			{"left": "300px"},
-			500,
-			function(){
-				$(this).hide();
-				$(this).css("left", "-212px");
-			}
-		);
-	})
-	.mouseup (function(){
-		$(".ryu-throwing").hide();
-		$(".ryu-ready").show();
-		$(".ryu-cool").hide();
-		$(".ryu-still").hide();
-	});
-	$("body").keydown(function(){
-		if(event.which === 88) {
-			$(".ryu-still").hide();
-			$(".ryu-ready").hide();
-			$(".ryu-throwing").hide();
-			$(".ryu-cool").show();
-		}
-	})
-	.keyup(function(){
-		$(".ryu-cool").hide();
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").hide();
-		$(".ryu-still").show();
-	});
-});	
 
-function playHadouken () {
-	$("#hadouken-sound") [0].volume = 0.5;
-	$("#hadouken-sound") [0].load();
-	$("#hadouken-sound") [0].play();
-}
+//Allows Enter Keystroke
+  //$('#entry').mouseenter(function() {
+    //alert('mouse entered .ryu div');
+$('#btnName').click(function(){
+    var text = $('#inputName').val() + '<button>x</button>';
+    if(text.length){
+        $('<li />', {html: text}).appendTo('ul.justList')
+    }
+});
+
+$('ul').on('click','button' , function(el){
+    $(this).parent().remove()
+});
+});
+  
+	
+
+
+//event that marks throug a list item - double click
+
+//event that fades out unwanted items
+
+
